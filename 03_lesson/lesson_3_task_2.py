9fe8698235a39ee7b16158d8dcfa4217e7ff36d5
Irina Skripnikova <isokolova360@gmail.com>
2026-01-19 09:17:33 +0700

    third lesson commit_3

---
+++ 03_lesson/lesson_3_task_2.py
@@ -0,0 +1,11 @@
+from smartphone import Smartphone
+
+catalog = [Smartphone("Samsung", "S22 Ultra", "+79990001122"),
+           Smartphone("POCO", "X22", "+73330001144"),
+           Smartphone("Apple", "Iphone 17", "+71112221122"),
+           Smartphone("Huawei", "Mate", "+75550001134"),
+           Smartphone("Nokia", "N90", "+76660001114"),
+           ]
+
+for i in catalog:
+    print(f"{i.brand} - {i.model}, {i.phone_number}")
