9351783c08ec94e982d925fda269b7b8474a861d
Oleg Skripnikov <osv719@gmail.com>
2026-01-30 20:09:51 +0700

    task_03

--- 05_lesson/lesson05_task3.py
+++ 05_lesson/lesson05_task3.py
@@ -2,12 +2,17 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 
 # Регистрируем драйвер для Firefox
-driver = webdriver.Chrome()
+driver = webdriver.Firefox()
 driver.maximize_window()
 
 # Заходим на страницу
 driver.get("http://the-internet.herokuapp.com/inputs")
 
 # Ищем элемент и взаимодействуем с ним
-button = driver.find_element(By.CSS_SELECTOR, "button.btn-primary")
-button.click()
+input_form = driver.find_element(By.CSS_SELECTOR, "input")
+input_form.send_keys("Sky")
+input_form.clear()
+input_form.send_keys("Pro")
+
+# Завершаем работу
+driver.quit()
