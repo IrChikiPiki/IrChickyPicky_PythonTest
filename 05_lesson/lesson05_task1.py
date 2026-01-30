1862d3ac4c251789bb7fc70cbf6f68415136b85c
Oleg Skripnikov <osv719@gmail.com>
2026-01-30 19:56:33 +0700

    task_02

--- 05_lesson/lesson05_task1.py
+++ 05_lesson/lesson05_task1.py
@@ -1,13 +1,8 @@
-from time import sleep
-
 from selenium import webdriver
 from selenium.webdriver.common.by import By
-from selenium.webdriver.common.keys import Keys
 
 driver = webdriver.Chrome()
 driver.maximize_window()
 driver.get("http://uitestingplayground.com/classattr")
 button = driver.find_element(By.CSS_SELECTOR, "button.btn-primary")
 button.click()
-
-sleep(3)
---
+++ 05_lesson/lesson05_task2.py
@@ -0,0 +1,8 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+
+driver = webdriver.Chrome()
+driver.maximize_window()
+driver.get(" http://uitestingplayground.com/dynamicid")
+button = driver.find_element(By.CSS_SELECTOR, "button.btn-primary")
+button.click()
