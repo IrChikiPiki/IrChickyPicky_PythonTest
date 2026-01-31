9307d164dd49c383438f00ea1d353dcddc13d4c0
Oleg Skripnikov <osv719@gmail.com>
2026-01-31 18:01:33 +0700

    lesson_6_task_01

---
+++ 06_lesson/lesson06_task1.py
@@ -0,0 +1,19 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+
+# Регистрируем драйвер для Firefox
+driver = webdriver.Firefox()
+driver.maximize_window()
+
+# Заходим на страницу
+driver.get("http://uitestingplayground.com/ajax")
+driver.implicitly_wait(16)
+
+# Нажимаем на синюю кнопку
+blue_btn = driver.find_element(By.CSS_SELECTOR, "#ajaxButton").click()
+
+green_form = driver.find_element(By.CSS_SELECTOR, "p.bg-success").text
+
+print(green_form)
+
+driver.quit()
